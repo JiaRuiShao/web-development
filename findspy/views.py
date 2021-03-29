ebf84802a1a82b1b256d4c74b1ee8731c0b8f3f6
Jessie Shao <jiaruishaojessica@gmail.com>
2021-03-29 01:52:59 -0400

    updated the login html and login action in views.py so that the inputs fit the overall theme (may not further discussion)

--- findspy/views.py
+++ findspy/views.py
@@ -13,25 +13,78 @@ def home(request):
     context = {'page_name': 'Home'}
     return render(request, 'findspy/home.html', context)
 
-
 @login_required
 def self_profile_action(request):
     return None
-    
+
+
+@login_required
 def create_room():
     return None
 
 
+@login_required
 def join_room():
     return None
 
 
-def view_profile():
-    return None
+@login_required
+def view_profile(request, user_id):
+    profile = Profile.objects.get(user__id=user_id)
+    context = {'page_name': 'Profile',
+               # 'profile_form': ProfileForm(instance=Profile.objects.all().get(user__id=user_id)),
+               'profile': profile,
+               }
+    # for viewing and editing my profile
+    if user_id == request.user.id:
+        if request.method == 'POST':
+            profile_form = ProfileForm(request.POST, request.FILES)
+            if not profile_form.is_valid():
+                context['message'] = 'The data input is not valid. Try again.'
+                return render(request, 'findspy/profile.html', context)
+            # pic = profile_form.cleaned_data['picture']
+            # print('Uploaded picture: {} (type={})'.format(pic, type(pic)))
+
+            profile.picture = profile_form.cleaned_data['picture']
+            profile.bio = profile_form.cleaned_data['bio']
+            profile.content_type = profile_form.cleaned_data['picture'].content_type
+            profile.save()
+            context['profile'] = profile
+
+        return render(request, 'findspy/profile.html', context)
+
+    # for viewing and following/unfollowing the others' profile
+    view_user = User.objects.get(id=user_id)
+    context['view_user'] = view_user
+
+    # request.user's profile object: request.user.profile_user
+    if view_user in request.user.profile_user.following.all():
+        context['following'] = True
+    else:
+        context['following'] = False
+
+    # if request.method == 'GET':
+    #     return render(request, 'socialnetwork/profile-view.html', context)
+
+    if request.method == 'POST':
+        if request.POST['follow'] == 'unfollow':
+            Profile.objects.get(user=request.user).following.remove(view_user)
+            context['following'] = False
+        elif request.POST['follow'] == 'follow':
+            Profile.objects.get(user=request.user).following.add(view_user)
+            context['following'] = True
+        else:
+            context['message'] = 'Error!'
+
+    return render(request, 'findspy/profile-view.html', context)
 
 
-def get_photo():
-    return None
+@login_required
+def get_photo(request, profile_id):
+    profile = Profile.objects.get(id=profile_id)
+    if not profile.picture:
+        raise Http404
+    return HttpResponse(profile.picture, content_type=profile.content_type)
 
 
 def login_action(request):
@@ -41,18 +94,21 @@ def login_action(request):
         context['form'] = LoginForm()
         return render(request, 'findspy/login.html', context)
 
-    # login
-    form = LoginForm(request.POST)
-    context['form'] = form
-
-    # Validates the form.
-    if not form.is_valid():
+    # get username and password
+    if 'username' not in request.POST or 'password' not in request.POST or not request.POST['username'] or not request.POST['password']:
+        context['error'] = "Invalid username/password."
         return render(request, 'findspy/login.html', context)
 
-    new_user = authenticate(username=form.cleaned_data['username'],
-                            password=form.cleaned_data['password'])
+    username = request.POST['username']
+    password = request.POST['password']
 
-    login(request, new_user)
+    # validate the user info
+    user = authenticate(username=username, password=password)
+    if not user:
+        context['error'] = "Invalid username/password."
+        return render(request, 'findspy/login.html', context)
+
+    login(request, user)
     return redirect(reverse('home'))
 
 
@@ -68,16 +124,16 @@ def register_action(request):
         context['form'] = RegisterForm()
         return render(request, 'findspy/register.html', context)
 
-    # Creates a bound form from the request POST parameters and makes the
+    # creates a bound form from the request POST parameters and makes the
     # form available in the request context dictionary
     form = RegisterForm(request.POST)
     context['form'] = form
 
-    # Validates the form
+    # validates the user
     if not form.is_valid():
         return render(request, 'findspy/register.html', context)
 
-    # After conformed the form data is valid. Register and login the user
+    # register and login the user
     new_user = User.objects.create_user(username=form.cleaned_data['username'],
                                         password=form.cleaned_data['password'],
                                         email=form.cleaned_data['email'], 
@@ -86,9 +142,10 @@ def register_action(request):
     new_user.save()
 
     # create profile
-    new_profile = Profile.objects.create(user=new_user)
+    new_profile = Profile(user=new_user)
     new_profile.save()
 
+    # authenticate and login the user
     new_user = authenticate(username=form.cleaned_data['username'],
                             password=form.cleaned_data['password'])
     login(request, new_user)
